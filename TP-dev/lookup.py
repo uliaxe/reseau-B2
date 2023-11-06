9cc8965f5b06bd5eedb4601f8e3125a561bff155
uliaxe <lukaschami3@gmail.com>
2023-11-06 18:53:23 +0100

    update lookup

--- TP-dev/lookup.py
+++ TP-dev/lookup.py
@@ -1,7 +1,5 @@
-import socket
-def lookup(domain_name):
-    try:
-        ip = socket.gethostbyname(domain_name)
-        return ip
-    except socket.gaierror:
-        return f"'{domain_name}' n'est pas un domaine valide."
+from socket import gethostbyname
+
+name = input("Enter a hostname: ")
+ip = gethostbyname(name)
+print("L'adresse IP de ", name, " est ", ip)
