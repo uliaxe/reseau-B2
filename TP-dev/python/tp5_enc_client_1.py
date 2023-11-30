da6c534c0589c2e347c2ba6d7b95ea45f5122e1f
uliaxe <lukaschami3@gmail.com>
2023-11-30 14:38:01 +0100

    maj tp5

--- TP-dev/python/tp5_enc_client_1.py
+++ TP-dev/python/tp5_enc_client_1.py
@@ -7,13 +7,14 @@ def valid_expression(expr):
     pattern = re.compile(r'^\d{1,5}\s*[\+\-\*]\s*\d{1,5}$')
     return bool(pattern.match(expr))
 
-def send_message(s , msg):
-    #encode le message et envoie sur la réseau avec une séquence de fin
+def send_message(s, msg):
+    # Encode le message et envoie sur le réseau avec une séquence de fin
     encoded_msg = msg.encode('utf-8')
     msg_length = len(encoded_msg)
     header = msg_length.to_bytes(4, byteorder='big')
     payload = header + encoded_msg + b'<clafin>'
     s.send(payload)
+
     
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('10.1.2.15', 13337))
