446171bf65ab968beed97c087c17ea8f5acd1e70
uliaxe <lukaschami3@gmail.com>
2023-11-30 16:48:08 +0100

    maj tp5

--- TP-dev/python/tp5_web_serv_1.py
+++ TP-dev/python/tp5_web_serv_1.py
@@ -1,19 +1,18 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class ServHTTP(BaseHTTPRequestHandler):
-    def get(self):
+    def GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b'<h1>Hello, je suis un vrai serveur HTTP !</h1>')
-        
-    def run():
-        port = 8080
-        serv_address = ('', port)
-        httpd = HTTPServer(serv_address, ServHTTP)
-        print('Serveur web démarré sur le port {port}')
-        httpd.serve_forever()
+        self.wfile.write(b'<h1>Hello je suis un serveur HTTP</h1>')
 
-    if __name__ == '__main__':
-        run()
-    
+def run():
+    port = 8000
+    serv_address = ('', port)
+    httpd = HTTPServer(serv_address, ServHTTP)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
