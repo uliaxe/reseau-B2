8b3505b3e64dab1b5d0cfb4c521d03b3521dc4ec
uliaxe <lukaschami3@gmail.com>
2023-11-30 14:01:35 +0100

    maj tp5

--- TP-dev/python/tp5_enc_server_1.py
+++ TP-dev/python/tp5_enc_server_1.py
@@ -1,42 +1,32 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('127.0.0.1', 9999))
-sock.listen()
-client, client_addr = sock.accept()
-
-while True:
-    # On lit les 4 premiers octets qui arrivent du client
-    # Car dans le client, on a fixé la taille du header à 4 octets
-    header = client.recv(4)
+def rec_msg (client_socket):
+    #lit l'en tete pour definir la taille du message
+    header = client_socket.recv(4)
     if not header:
-        break
-
-    # On lit la valeur
-    msg_len = int.from_bytes(header[0:4], byteorder='big')
-
-    print(f"Lecture des {msg_len} prochains octets")
-
-    # Une liste qui va contenir les données reçues
+        return None
+    
+    #lit la valeur de la taille message depuis l'en tete
+    msg_length = int.from_bytes(header, byteorder='big')
+    
+    print(f"Reading {msg_length} bytes")
+    
+    #lit les octets suivants pour reconstruire le message
+    
     chunks = []
-
-    bytes_received = 0
-    while bytes_received < msg_len:
-        # Si on reçoit + que la taille annoncée, on lit 1024 par 1024 octets
-        chunk = client.recv(min(msg_len - bytes_received,
-                                1024))
+    bytes_recd = 0
+    
+    while bytes_recd < msg_length:
+        #lit 1024 octets ou moins à la fois
+        chunk = client_socket.recv(min(msg_length - bytes_recd, 1024))
         if not chunk:
-            raise RuntimeError('Invalid chunk received bro')
-
-        # on ajoute le morceau de 1024 ou moins à notre liste
+            raise RuntimeError('Invalid chunk received')
+        
+        #ajouter le morceau à la liste
         chunks.append(chunk)
-
-        # on ajoute la quantité d'octets reçus au compteur
-        bytes_received += len(chunk)
-
-    # ptit one-liner pas combliqué à comprendre pour assembler la liste en un seul message
-    message_received = b"".join(chunks).decode('utf-8')
-    print(f"Received from client {message_received}")
-
-client.close()
-sock.close()
+        
+        #ajoute le morceau à la liste
+        bytes_recd += len(chunk)
+        
+        #assemble    
+    
