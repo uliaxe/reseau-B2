416647635b78b77f2b260f3fe8267c822f3606b0
uliaxe <lukaschami3@gmail.com>
2023-12-01 14:07:11 +0100

    maj tp5

---
+++ TP-dev/python/tp5_web_client_1.py
@@ -0,0 +1,34 @@
+import http.client
+
+def simple_http_client():
+    # Adresse du serveur
+    server_address = "localhost"
+    port = 13337
+
+    # Création d'une connexion au serveur
+    connection = http.client.HTTPConnection(server_address, port)
+
+    try:
+        # Envoi d'une requête GET
+        connection.request("GET", "/")
+
+        # Récupération de la réponse du serveur
+        response = connection.getresponse()
+
+        # Affichage de la réponse
+        print("Code de réponse:", response.status)
+        print("En-têtes de réponse:")
+        for header, value in response.getheaders():
+            print(f"{header}: {value}")
+
+        # Affichage du corps de la réponse
+        body = response.read().decode("utf-8")
+        print("\nCorps de la réponse:")
+        print(body)
+
+    finally:
+        # Fermeture de la connexion
+        connection.close()
+
+if __name__ == "__main__":
+    simple_http_client()
