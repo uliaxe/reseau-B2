d036e2c84f882e12c6fb32665edf08e224ad9e32
uliaxe <lukaschami3@gmail.com>
2023-12-08 14:48:32 +0100

    upd

--- TP6/python/chat_server_ii_4.py
+++ TP6/python/chat_server_ii_4.py
@@ -1,44 +1,66 @@
 import asyncio
 
-async def send_messages(writer):
-    while True:
-        # Saisie utilisateur
-        message = input("Entrez votre message (ou Ctrl+C pour quitter): ")
+# Variable globale pour stocker les informations des clients
+CLIENTS = {}
 
-        # Envoi du message au serveur
-        writer.write(message.encode())
-        await writer.drain()
+async def handle_client(reader, writer):
+    # Obtenez les informations sur le client
+    client_addr = writer.get_extra_info('peername')
 
-async def receive_messages(reader):
-    while True:
-        # Réception des messages du serveur
-        data = await reader.read(1024)
-        if not data:
-            break
+    # Vérifie si le client est déjà connecté
+    if client_addr in CLIENTS:
+        print(f"Client déjà connecté: {client_addr}")
+        return
 
-        message = data.decode()
-        print(f"Message reçu : {message}")
+    print(f"Client connecté: {client_addr}")
 
-async def main():
-    # Définir l'adresse et le port du serveur
-    server_address = ('10.1.2.20', 13337)
+    # Stocke les informations du client dans CLIENTS
+    CLIENTS[client_addr] = {"r": reader, "w": writer}
 
     try:
-        # Connexion au serveur
-        reader, writer = await asyncio.open_connection(*server_address)
+        while True:
+            # Recevez le message du client
+            data = await reader.read(1024)
+            if not data:
+                break
 
-        # Exécuter les tâches en parallèle
-        await asyncio.gather(
-            send_messages(writer),
-            receive_messages(reader)
-        )
+            message = data.decode()
 
-    except KeyboardInterrupt:
-        pass
+            # Diffuser le message à tous les clients
+            await broadcast_message(client_addr, message)
+
     finally:
-        # Fermer la connexion après Ctrl+C
-        if not writer.is_closing():
-            writer.close()
+        # Retirez le client de CLIENTS lorsqu'il se déconnecte
+        del CLIENTS[client_addr]
+        print(f"Client déconnecté: {client_addr}")
+
+async def broadcast_message(sender_addr, message):
+    # Formatage du message pour la diffusion
+    sender_ip, sender_port = sender_addr
+    broadcast_msg = f"{sender_ip}:{sender_port} a dit : {message}"
+
+    # Envoi du message à tous les clients sauf à l'expéditeur
+    for addr, client_info in CLIENTS.items():
+        if addr != sender_addr:
+            writer = client_info["w"]
+            writer.write(broadcast_msg.encode())
+            await writer.drain()
+
+async def main():
+    # Créer le serveur
+    server = await asyncio.start_server(
+        handle_client, '10.1.2.20', 13337)
+
+    # Obtenir les informations d'adresse du serveur
+    addr = server.sockets[0].getsockname()
+    print(f'Serveur en écoute sur {addr}')
+
+    async with server:
+        # Attendre indéfiniment que le serveur soit fermé
+        await server.serve_forever()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
